1ee3e08ea88c5aa6841a1c8f2b88a241d50147f3
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2019-01-15 23:52:15 +0100

    starting with adding posts

--- app/posts/store.php
+++ app/posts/store.php
@@ -3,7 +3,43 @@
 declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
-
+var_dump($_FILES['post-img']);
 // In this file we store/insert new posts in the database.
+if (isset($_FILES['post-img'])){
 
-redirect('/');
+  $post = $_FILES['post-img'] ;
+  $description = $_POST['description'];
+
+  if ($post['type'] === 'image/jpeg' || $post['type'] === 'image/png' || $post['type'] === 'image/gif'){
+    if ($post['size'] < 3000000){
+      $id = $_SESSION['user']['id'];
+      $path = '/../img/post_img/';
+      $postName = time().'-'.$id.'-'.$post['name'];
+      $createdAt = date("Y-m-d H:i:s");
+      move_uploaded_file($post['tmp_name'], __DIR__.$path.$postName);
+
+      $query = 'INSERT INTO posts (user_id, post_img, description, created_at)
+       VALUES (:user_id, :post_img, :description, :created_at)';
+
+      $statement = $pdo->prepare($query);
+
+      $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
+      $statement->bindParam(':post_img', $postName, PDO::PARAM_STR);
+      $statement->bindParam(':description', $description, PDO::PARAM_STR);
+      $statement->bindParam(':created_at', $createdAt, PDO::PARAM_STR);
+      $statement->execute();
+
+      $post = $statement->fetch(PDO::FETCH_ASSOC);
+      if (!$statement) {
+          die(var_dump($pdo->errorInfo()));
+      };
+
+      $_SESSION['user']['post-img'] = $postName;
+      $_SESSION['user']['description'] = $description;
+
+
+       redirect('/../../index.php/');
+    }
+  }
+
+}
