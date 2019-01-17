93b7ead9a3122f65041e554c8ee3631bd3241729
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2019-01-17 21:22:12 +0100

    edit and delete post added

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<?php require __DIR__.'/views/header.php'; ?>
+<?php require __DIR__.'/views/header.php';
 
-<?php if (!isset($_SESSION['user'])): ?>
+if (!isset($_SESSION['user'])):
+?>
 <div class="login-container">
   <div class="login-wrap">
     <div class="photoify">
       <h1>Photoify</h1>
     </div>
+
+    <!-- LONGIN FORM -->
     <form action="app/users/login.php" method="post">
         <div class="login-html">
           <input a href="#"id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
@@ -31,10 +34,11 @@
                 <label class="login" style="color: #fff;" for="tab-2">Sign up</a>
               </div>
             </div>
-    </form>
+        </form>
 
-    <form action="app/users/login.php" method="post">
-        <div class="sign-up-htm">
+        <!-- SIGN UP FORM -->
+        <form action="app/users/login.php" method="post">
+          <div class="sign-up-htm">
             <div class="group">
               <label for="pass" class="label">Email Address</label>
               <input class="input" type="email" name="email-reg" id="email-r" placeholder="your.email@email.com" required>
@@ -64,47 +68,69 @@
             <label class="login" style="color: #fff;" for="tab-1">Login</a>
           </div>
         </div>
-    </form>
-
-      </div>
-    </div>
+      </form>
+     </div>
+   </div>
   </div>
-</div>
+</div><!--/ container -->
 
-<?php else: ?>
-    <div class="container">
-        <div class="new-post">
-            <a href="/views/add-psot.php" class="badge badge-light"><i class="fa fa-plus"></i> Share a new post</a>
-        </div>
+<?php else:
+
+    $posts = getPostInfo($pdo);
+    $posts = array_reverse($posts);
+    $id = $_SESSION['user']['id'];
+?>
+
+<!-- POST CONTAINER -->
+<div class="container">
+    <div class="new-post">
+        <a href="/views/add-post.php" class="badge badge-light"><i class="fa fa-plus"></i> Share a new post</a>
+    </div>
+    <?php foreach ($posts as $post):?>
+
+        <!-- FEED -->
      <div class="row">
       <div class="col-lg-6 offset-lg-3">
        <div class="post shadow-lg bg-white">
         <div class="post-heading">
          <div class="media m-0">
-             <!-- <em class="fa fa-ellipsis-h"></em> -->
           <div class="d-flex mr-3">
-           <a href=""><img class="img-fluid rounded-circle" src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/4.jpg" alt="User"></a>
+          <?php if (isOwnerOfPost($post['user_id'], $id)): ?>
+            <a href="/views/edit-post.php"><i class="far fa-edit"></i></a>
+          <?php endif; ?>
+          <a href=""><img class="img-fluid rounded-circle" src="/app/img/profile_img/<?php echo $post['profile_img']?>" alt="User"></a>
           </div>
           <div class="media-body">
-           <p class="m-0">Benjamin Robinson</p>
-           <small><span><i class="icon ion-md-time"></i> 10 hours ago</span></small>
+           <p class="m-0"><?php echo $post['username']?></p>
+           <small><span><i class="icon ion-md-time"></i> <?php echo $post['created_at']?></span></small>
           </div>
          </div><!--/ media -->
         </div><!--/ post-heading -->
-
         <div class="post-item">
-         <img class="img-fluid" src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/1.jpg" alt="Image">
+         <img class="img-fluid" src="/app/img/post_img/<?php echo $post['post_img']; ?>" alt="Image">
+         <label><?php echo $post['description']; ?></label>
+         <!-- FEED END-->
+
+         <!-- DELETE POST -->
+         <div class="delete-post">
+             <form action="app/posts/delete.php" method="post" enctype="multipart/form-data">
+             <button class="btn btn-primary">Like <i class="far fa-thumbs-up"></i> </button>
+             <button class="btn btn-primary">180 Likes</button>
+             <?php if (isOwnerOfPost($post['user_id'], $id)): ?>
+            <input type="hidden" name="postId" value="<?php echo $post['post_id'];?>" >
+            <button type="submit" name="delete-post" class="btn btn-primary" >Delete</button>
+            </form>
+            <?php endif; ?>
+            <!-- DELETE POST END-->
+
+         </div>
         </div><!--/ post-item -->
-        <!-- <i class="fa fa-heart"></i> -->
-
-
-
        </div><!--/ post -->
+      </div>
+     </div><!--/ col-lg-6 -->
+    <?php endforeach; ?>
+    </div><!--/ row -->
 
-      </div><!--/ col-lg-6 -->
-     </div><!--/ row -->
-    </div><!--/ container -->
+<?php endif;
 
-<?php endif; ?>
-
-<?php require __DIR__.'/views/footer.php'; ?>
+require __DIR__.'/views/footer.php'; ?>
