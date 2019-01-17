f1f78545ea388e7a667d073b5fe8888caef4552d
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2019-01-17 21:50:54 +0100

    delete and edit fixed

--- app/posts/update.php
+++ app/posts/update.php
@@ -7,19 +7,20 @@ require __DIR__.'/../autoload.php';
 // In this file we delete posts in the database.
 
 
-if (isset($_POST['changeDescription'], $_POST['postId']))
+if (isset($_POST['description-update'], $_POST['post-id']))
 {
-  $descriptionUpdate = trim(filter_var($_POST['changeDescription'], FILTER_SANITIZE_STRING));
+  $descriptionUpdate = trim(filter_var($_POST['description-update'], FILTER_SANITIZE_STRING));
 
-  $postId = trim(filter_var($_POST['postId']));
+  $postId = trim(filter_var($_POST['post-id']));
 
   $updateStatement = $pdo->prepare('UPDATE posts SET description = :description WHERE id = :id');
+
   if (!$updateStatement)
         {
             die(var_dump($pdo->errorInfo()));
         }
 
-  $updateStatement->bindParam(':description', $newDescription, PDO::PARAM_STR);
+  $updateStatement->bindParam(':description', $descriptionUpdate, PDO::PARAM_STR);
   $updateStatement->bindParam(':id', $postId, PDO::PARAM_INT);
   $updateStatement->execute();
 
