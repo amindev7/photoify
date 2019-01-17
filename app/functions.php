d33ddce37829c5f5d62715ac3497ff3c85dad5a5
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2019-01-17 21:46:59 +0100

    get post info function added

--- app/functions.php
+++ app/functions.php
@@ -19,7 +19,7 @@ if (!function_exists('redirect')) {
 
 /**
  * [alert description]
- * @return string [description]
+ * @return [type] [description]
  */
 function alert()
 {
@@ -27,4 +27,40 @@ function alert()
 		echo '<div class="alert alert-primary" role="alert">'.$_SESSION['success'].'</div>';
 		unset($_SESSION['success']);
     }
+}
+/**
+ * [isOwnerOfPost description]
+ * @param  int  $post [description]
+ * @param  int  $user [description]
+ * @return bool       [description]
+ */
+
+function isOwnerOfPost(int $post, int $user): bool
+{
+  return $post === $user;
+}
+
+/**
+ * [getPostInfo description]
+ * @param  PDO   $pdo [description]
+ * @return array      [description]
+ */
+
+function getPostInfo(PDO $pdo) :array
+{
+    $query = 'SELECT p.id as post_id,
+    p.description, p.created_at, p.post_img,
+    u.username, u.id as user_id, u.profile_img
+    FROM posts p
+    INNER JOIN users u
+    WHERE u.id = p.user_id';
+
+    $statement = $pdo->prepare($query);
+    if (!$statement)
+    {
+        die(var_dump($pdo->errorInfo()));
+    }
+    $statement->execute();
+
+    return $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 }
