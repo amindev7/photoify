333daf0789f0a70e74473dd635349c57bac49530
Amin Mohamed <42947739+Amin-Mohamed@users.noreply.github.com>
2019-01-21 02:01:49 +0100

    start with like function logic

---
+++ app/posts/like.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+
+if (isset($_POST['post_id']))
+{
+	if (filter_var($_POST['post_id'], FILTER_VALIDATE_INT))
+	{
+		$userId = $_SESSION['user']['id'];
+		$postId = $_POST['post_id'];
+
+		$query = "SELECT * FROM likes
+        WHERE
+        user_id = :user_id AND post_id = :post_id";
+		$statement = $pdo->prepare($query);
+
+		if (!$statement)
+		{
+			die(var_dump($pdo->errorInfo()));
+		}
+		$statement->bindParam(':user_id', $user, PDO::PARAM_INT);
+		$statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+		$statement->execute();
+		$likes = $statement->fetch(PDO::FETCH_ASSOC);
+
+	}
+}
